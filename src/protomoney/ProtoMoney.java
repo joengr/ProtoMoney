8cf2b3f4a8a31b6a627d8f0c33fa37cd4f9fe5b6
jrodrig1 <jrodrig1@JRODRIG1-004847.cscinfo.com>
2018-09-05 13:21:11 -0400

    Test to upload to GiT

---
+++ src/protomoney/ProtoMoney.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package protomoney;
+
+/**
+ *
+ * @author jrodrig1
+ */
+public class ProtoMoney {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here, veremos que hacemos con este codigo
+    }
+    
+}
