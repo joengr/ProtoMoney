0f92f20489078af0abfecdda79f68631998fa458
jrodrig1 <jrodrig1@JRODRIG1-004847.cscinfo.com>
2018-09-07 12:07:14 -0400

    Test to upload to GiT

--- src/protomoney/ProtoMoney.java
+++ src/protomoney/ProtoMoney.java
@@ -10,30 +10,34 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import org.apache.derby.jdbc.EmbeddedDriver;
+import java.sql.ResultSetMetaData;
+//import org.apache.derby.jdbc.EmbeddedDriver; Driver para la base de datos incluida
 
-/**
- *
- * @author jrodrig1
- */
 public class ProtoMoney {
-
     /**
      * @param args the command line arguments
      */
+    private static String dbnombre = "jdbc:derby:ProtoMoney; create = true; user= apps; password= apps";
+    private static Connection protoConnObj = null; //Allows to connect to DB
+    private static Statement protoStatObj=null; //Allows to run SQL procedures
+    private static ResultSet protoResObj=null; //Stores the SQL procedures data
+    
     public static void main(String[] args) {
-        // TODO code application logic here, veremos que hacemos con este codigo
-    Connection protoConnObj = null; //Allows to connect to DB
-    Statement protoStatObj=null; //Allows to run SQL procedures
-    ResultSet protoResObj=null; //Stores the SQL procedures data
+       // Solo nombrando los procedimientos, todos estan definidos abajo
+        createConnection();
+       // insertTabla();
+       // DespliegaTabla();
+       // termina();
+    }    
+    //Crea una conexion a la base de datos usando los argumentos abajo    
+    private static void createConnection()
+    {
     try{
-        protoConnObj = DriverManager.getConnection("jdbc:derby://localhost:1527:proto2", "apps", "apps");
-        
+        protoConnObj = DriverManager.getConnection(dbnombre);
+        //protoConnObj = DriverManager.getConnection("jdbc:derby://localhost:1527/proto2; create = true; user= apps; password= apps");
     }
     catch (SQLException e){
     e.printStackTrace();
     }
-    
     }
-    
-}
+    }
