5cb97f73a6b965c502499a3964c621bd71e2a7da
jrodrig1 <jrodrig1@JRODRIG1-004847.cscinfo.com>
2018-09-10 13:45:24 -0400

    Test to upload to GiT

--- src/protomoney/ProtoMoney.java
+++ src/protomoney/ProtoMoney.java
@@ -5,39 +5,58 @@
  */
 package protomoney;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.sql.ResultSetMetaData;
+import java.sql.*;
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.ResultSet;
+//import java.sql.SQLException;
+//import java.sql.Statement;
+//import java.sql.ResultSetMetaData;
 //import org.apache.derby.jdbc.EmbeddedDriver; Driver para la base de datos incluida
 
 public class ProtoMoney {
     /**
      * @param args the command line arguments
      */
-    private static String dbnombre = "jdbc:derby:ProtoMoney; create = true; user= apps; password= apps";
+    private static String dbnombre = "jdbc:derby://localhost:1527/Proto2; user= app; password= app";
     private static Connection protoConnObj = null; //Allows to connect to DB
-    private static Statement protoStatObj=null; //Allows to run SQL procedures
+    private static Statement StatObj=null; //Allows to run SQL procedures
     private static ResultSet protoResObj=null; //Stores the SQL procedures data
     
     public static void main(String[] args) {
        // Solo nombrando los procedimientos, todos estan definidos abajo
-        createConnection();
-       // insertTabla();
+       createConnection();
+       insertTabla();
        // DespliegaTabla();
        // termina();
-    }    
+    } 
+    
     //Crea una conexion a la base de datos usando los argumentos abajo    
-    private static void createConnection()
-    {
+    private static void createConnection(){
     try{
         protoConnObj = DriverManager.getConnection(dbnombre);
-        //protoConnObj = DriverManager.getConnection("jdbc:derby://localhost:1527/proto2; create = true; user= apps; password= apps");
+        }
+    catch (SQLException e)
+    {
+        e.printStackTrace();
     }
-    catch (SQLException e){
-    e.printStackTrace();
     }
+    
+    //Crea una secuencia de comandos para insertar en una tabla    
+    private static void insertTabla(){
+        String tabla = "PM_ACC_TYPE";
+        String Nombre = "DE2";
+        String tipo = "Debito con chequera";
+    //aqui va la secuencia para definir el Statement e insertar en la tabla
+    try {
+        Statement StatObj2 = protoConnObj.createStatement();
+        StatObj2.execute("insert into " + tabla + " values ('" + Nombre + "', '" + tipo + "')");
+        StatObj2.close();
+        }
+    catch (SQLException e)
+        {
+        e.printStackTrace();
+        }
     }
+    
     }
