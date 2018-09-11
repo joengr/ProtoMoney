6283579c2c608a99520e600a84606d5024a8e68a
jrodrig1 <jrodrig1@JRODRIG1-004847.cscinfo.com>
2018-09-11 15:52:27 -0400

    Test to upload to GiT

--- src/protomoney/ProtoMoney.java
+++ src/protomoney/ProtoMoney.java
@@ -6,12 +6,6 @@
 package protomoney;
 
 import java.sql.*;
-//import java.sql.Connection;
-//import java.sql.DriverManager;
-//import java.sql.ResultSet;
-//import java.sql.SQLException;
-//import java.sql.Statement;
-//import java.sql.ResultSetMetaData;
 import org.apache.derby.jdbc.EmbeddedDriver;
 
 public class ProtoMoney {
@@ -22,20 +16,19 @@ public class ProtoMoney {
     private static Connection protoConnObj = null; //Allows to connect to DB
     private static Statement StatObj=null; //Allows to run SQL procedures
     private static ResultSet protoResObj=null; //Stores the SQL procedures data
-    
+        
     public static void main(String[] args) {
-       // Solo nombrando los procedimientos, todos estan definidos abajo
+    // Solo nombrando los procedimientos, todos estan definidos abajo
        createConnection();
        creaTabla();
-       //insertTabla();
+       insertTabla();
        // DespliegaTabla();
-       // termina();
+       termina();
     } 
     
     //Crea una conexion a la base de datos usando los argumentos abajo    
     private static void createConnection(){
     try{
-        //Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
         protoConnObj = DriverManager.getConnection(dbnombre);
         }
     catch (SQLException e)
@@ -44,19 +37,13 @@ public class ProtoMoney {
     }
     }
     
-    //Crea una tabla llamada PM_ACC_TYPE
-    private static void creaTabla(){
-    String tabla = "CREATE TABLE pm_acc_type (\n"
-                + " codigo char(3) NOT NULL, \n"
-                + " descrip char(30)\n"
-                + ")";
-
-    //aqui va la secuencia para definir el Statement y crear la tabla
+    private static void creaTabla(){ //Procedimiento para crear una tabla embebida en la DB definida
+        String tablas = "CREATE TABLE uno(dos char(3))"; //Definicion de la tabla y su estructura
     try 
         {
-        protoConnObj = DriverManager.getConnection(dbnombre);
-        Statement StatObj3 = protoConnObj.createStatement();
-        StatObj3.execute(tabla);
+        protoConnObj = DriverManager.getConnection(dbnombre); //Asignacion de la conexion a la base de datos en dbnombre
+        Statement StatObj3 = protoConnObj.createStatement(); //Asignacion de estatuto hacia la conexion a la DB
+        StatObj3.execute(tablas); // Ejecucion del estatuto segun la variable 'tablas' definida arriba
         }
     catch (SQLException e)
         {
@@ -67,19 +54,32 @@ public class ProtoMoney {
     
     //Crea una secuencia de comandos para insertar en una tabla    
     private static void insertTabla(){
-        String tabla = "PM_ACC_TYPE";
-        String Nombre = "DE2";
-        String tipo = "Debito con chequera";
+        String tabla = "uno";
+        String Nombre = "DDD";
+        String pone = String.format("insert into %s values (\'%s\')", //Formato de una variable string para meter los datos string en variables
+                                    tabla, Nombre);
+      //String campo = pasa; no se necesitan los nombres de los campos
+
     //aqui va la secuencia para definir el Statement e insertar en la tabla
     try {
         Statement StatObj2 = protoConnObj.createStatement();
-        StatObj2.execute("insert into " + tabla + " values ('" + Nombre + "', '" + tipo + "')");
+        StatObj2.execute(pone);
         StatObj2.close();
         }
     catch (SQLException e)
         {
         e.printStackTrace();
         }
+    }
+    
+    private static void termina(){ //Da de baja la base de datos y el servicio
+    try{
+        protoConnObj = DriverManager.getConnection("jdbc:derby:Protoss;shutdown=true");
+        }
+    catch (SQLException e)
+    {
+        e.printStackTrace();
+    }
     }
     
     }
