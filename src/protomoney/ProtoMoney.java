940fe9b07f1088372083d63099e1d9e7c0d182a9
jrodrig1 <jrodrig1@JRODRIG1-004847.cscinfo.com>
2018-09-13 16:30:19 -0400

    New code without too much DB (removed create table as we can now interact directly within netbeans DB manager)

--- src/protomoney/ProtoMoney.java
+++ src/protomoney/ProtoMoney.java
@@ -6,6 +6,7 @@
 package protomoney;
 
 import java.sql.*;
+import java.util.Scanner;
 import org.apache.derby.jdbc.EmbeddedDriver;
 
 public class ProtoMoney {
@@ -16,16 +17,46 @@ public class ProtoMoney {
     private static Connection protoConnObj = null; //Allows to connect to DB
     private static Statement StatObj=null; //Allows to run SQL procedures
     private static ResultSet protoResObj=null; //Stores the SQL procedures data
-        
+    private static boolean salida = true;
+    private Integer opcion;
+    
     public static void main(String[] args) {
     // Solo nombrando los procedimientos, todos estan definidos abajo
        createConnection();
-       //creaTabla();
-       //llamaopicon();
-       metedb("2018-06-24", "BoA Checking", "Dos Mas", "Para mas pagos apropiados", -25.63);
-       despliegaTabla();
+       while (salida){
+       llamaopcion();
        //termina();
+       }
     } 
+    
+    private static void llamaopcion(){
+    Scanner reader = new Scanner(System.in);     //Nueva instancia de la clase Scanner
+                
+    System.out.println("Seleccione la opcion a utilizar ");
+    System.out.println("===============================");
+    System.out.println("Nueva entrada:       1");
+    System.out.println("Consultar valores:   2");
+    System.out.print("Seleccion: ");    
+    int n = reader.nextInt();  //Esta es la linea para leer lo que el usuario teclea
+   // System.out.print("leido: ");
+    switch (n){
+        case 1:
+            metedb("Santander Cheques","2018-08-19", "Jose Rodriguez", "Sueldo del 22 de Agosto", 4087.53, "SBT");
+            salida = false;
+            break;
+        case 2:
+            despliegaTabla();
+            salida = false;
+            break;
+        default:
+            System.out.println("Numero no valido");
+            salida = true;
+            break;
+    }
+    
+    }
+    
+    
     
     //Crea una conexion a la base de datos usando los argumentos abajo    
     private static void createConnection(){
@@ -38,39 +69,23 @@ public class ProtoMoney {
     }
     }
     
-    private static void creaTabla(){ //Procedimiento para crear una tabla embebida en la DB definida
-        String tablas = "CREATE TABLE uno(dos char(3))"; //Definicion de la tabla y su estructura
-    try 
-        {
-        protoConnObj = DriverManager.getConnection(dbnombre); //Asignacion de la conexion a la base de datos en dbnombre
-        Statement StatObj3 = protoConnObj.createStatement(); //Asignacion de estatuto hacia la conexion a la DB
-        StatObj3.execute(tablas); // Ejecucion del estatuto segun la variable 'tablas' definida arriba
-        }
-    catch (SQLException e)
-        {
-        e.printStackTrace();
-        }
-    }
-    
-    public static void metedb (String fecs, String cues, String clts, String descs, Double cats){
-        String tabla = "PM_REGISTRO";
-        String fecha = fecs;
+    public static void metedb (String cues, String fecs, String clts, String descs, Double cats, String SubC){
+        String tabla = "PM_REGISTRY";
         String cuenta = cues;
+        String fecha = fecs;
         String cliente = clts;
         String desc = descs;
         Double cnts = cats;
-        Integer sigue = 1;
+        String catego = SubC;
+
       //String campo = pasa; no se necesitan los nombres de los campos
 
     //aqui va la secuencia para definir el Statement e insertar en la tabla
     try {
         Statement StatObj2 = protoConnObj.createStatement();
-        ResultSet protoResObj = StatObj2.executeQuery("select * from app.pm_registro");
-        while (protoResObj.next()) {
-               sigue ++; 
-               }
-        String pone = String.format("insert into %s values (%s,\'%s\',\'%s\',\'%s\',\'%s\',%s,3654.51)", //Formato de una variable string para meter los datos string en variables
-                                    tabla, sigue, fecha, cuenta, cliente, desc, cnts);
+        //ResultSet protoResObj = StatObj2.executeQuery("select * from app.pm_registry"); no se necesita, estaba solo para leer e insertar al final
+        String pone = String.format("insert into %s values (\'%s\',\'%s\',\'%s\',\'%s\',%s,\'%s\')", //Formato de una variable string para meter los datos string en variables
+                                    tabla, cuenta, fecha, cliente, desc, cnts, catego);
         StatObj2.execute(pone);
         StatObj2.close();
         }
@@ -84,11 +99,11 @@ public class ProtoMoney {
         //String miras = String.format("insert into %s values (\'%s\')", tabla, Nombre);
     try{
         Statement StatObj2 = protoConnObj.createStatement();
-        ResultSet protoResObj = StatObj2.executeQuery("select * from app.pm_registro");
-        System.out.println("fecha:\t\t" +"cuenta:\t\t\t" +"cliente:\t\t"+"descripcion:\t\t" +"cantidad:\t\t"+"saldo:");
-        System.out.println("===============================================================================================================================");
+        ResultSet protoResObj = StatObj2.executeQuery("select * from app.test");
+        System.out.println("cuenta:\t\t" +"fecha:\t\t\t" +"cliente:\t\t"+"descripcion:\t\t" +"cantidad:\t\t"+"categoria: \t\t"+"subcategoria: ");
+        System.out.println("==================================================================================================================================================");
         while (protoResObj.next()) {
-            for (int i=2;i<8;i++){
+            for (int i=1;i<8;i++){
                System.out.print(protoResObj.getString(i)+ "\t"); 
             }
             System.out.println();
